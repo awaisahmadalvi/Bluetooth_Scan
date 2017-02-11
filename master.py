1214bb8dca7eb3e8d49d598a1d5dca66d7206287
root <awaisahmadalvi@gmail.com>
2017-02-11 08:02:39 -0500

    - DB table query updated - last entry time comparison in SQLite - Checking validity of bus tour (NEW or On-Station) - update datetime till bus is on-station - showing all DB Entries - INQM to max limit, showing mac devices continously

--- master.py
+++ master.py
@@ -1,4 +1,6 @@
-import serial, time
+#!/usr/bin/env python
+
+import serial, time, sqlite3
 import wiringpi2 as gpio
 
 def BLwrite(str):
@@ -39,7 +41,7 @@ def startInq():
         print res
         if "INQUIRING" not in res:
                 print BLwrite("AT+ROLE=1")
-                print BLwrite("AT+INQM=1,1,5")
+                print BLwrite("AT+INQM=1,10,48")
                 print BLwrite("AT+INIT")
                 print BLwrite("AT+INQ")
 
@@ -48,23 +50,44 @@ def parseInq(inq):
         inq = inq.replace(":","")
 	return inq
 
+def addToDB(mac):
+        global curs
+        found = 0
+        for row in curs.execute("SELECT * FROM BTmac WHERE tdate > datetime('now', '-5 seconds') AND mac=(?)", (mac,)):
+                found = 1
+                curs.execute("UPDATE BTmac SET tdate=datetime('now'), mac=(?) WHERE tdate=?", (mac,row[0]))
+                db.commit()
+                print "found"
+                print row
+                break
+        if found is 0:
+                print "not found"
+                curs.execute("INSERT INTO BTmac values(datetime('now'), (?))", (mac,))
+                found = 0
+                db.commit()
 
-def addToList(mac):
-	found = 0
-	for item in macList:
-		if mac in item:
-			found = 1
-			break
-	if found is 0:
-		macList.append(mac)
+def showDB():
+	print "Showing DB Entries"
+	global curs
+	for row in curs.execute("SELECT * FROM BTmac"):
+		print row
 
-def printList():
-	print "LIST ON"
-	for item in macList:
-		print item
-	print "LIST OFF"
+
+def createDBtable():
+	global curs
+	curs.execute("CREATE TABLE IF NOT EXISTS BTmac (tdate DATETIME, mac TEXT);")
+
+def connectDB():
+	global db
+	db=sqlite3.connect('busesMac.db')
+	global curs
+	curs=db.cursor()
 
 def main():
+
+	connectDB()
+	createDBtable()
+
 	setPin()
 	openBL()
 	print BLwrite("AT")
@@ -74,16 +97,24 @@ def main():
 		res = BLread()
 		time.sleep(0.1)
 		if "OK\r" in res :
-			printList()
+			showDB()
 			BLwrite("AT+INQ")
 		else :
 			if "+INQ" in res:
 				res = parseInq(res)
 				print res
-				addToList(res)
+				addToDB(res)
 #        print BLwrite("AT+INQC")
 	BLser.close()
 	gpio.digitalWrite(1,0)
 
 macList = []
 main()
+#connectDB()
+#createDBtable()
+#showDB()
+#t = time.localtime(time.time())
+##print t
+##t2 = time.localtime(time.time()+2)
+#print t2
+#print t2 - t
---
+++ showDB.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import sqlite3, time
+from datetime import time as dttime
+from datetime import datetime
+
+def showDB():
+	print "Showing DB Entries"
+	global curs
+	for row in curs.execute("SELECT * FROM BTmac"):
+		print row
+
+def addToDB(mac):
+        global curs
+	found = 0
+        for row in curs.execute("SELECT * FROM BTmac WHERE tdate > datetime('now', '-5 seconds') AND mac=(?)", (mac,)):
+		found = 1
+		curs.execute("UPDATE BTmac SET tdate=datetime('now'), mac=(?) WHERE tdate=?", (mac,row[0]))
+		db.commit()
+		print "found"
+                print row
+		break
+	if found is 0:
+		print "not found"
+		curs.execute("INSERT INTO BTmac values(datetime('now'), (?))", (mac,))
+		found = 0
+		db.commit()
+
+def createDBtable():
+	global curs
+	curs.execute("CREATE TABLE IF NOT EXISTS BTmac (tdate DATETIME, mac TEXT);")
+
+def connectDB():
+	global db
+	db=sqlite3.connect('busesMac.db')
+	global curs
+	curs=db.cursor()
+
+def main():
+	connectDB()
+	createDBtable()
+	addToDB("Hello1")
+	showDB()
+
+main()
+
+#t = datetime.now()
+#print t.hour print t.minute print t.second
+#t2 = dttime(5,22,0)
+#print t2 - t
+#datetime(2017,4,22,05,12,36)
